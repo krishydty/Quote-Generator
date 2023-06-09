765e1a7c4f61a4d64354f6954dd9fd2bd68c379d
krishydty <35625095+krishydty@users.noreply.github.com>
2023-06-09 21:15:39 +0100

    Added a local mock of quoteGenerator

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import React, {useState} from 'react'
+//import React, {useEffect, useState} from 'react'
 import Image from 'next/image'
 import styles from '@/styles/Home.module.css'
 import { GeneratedIdentifierFlags } from 'typescript'
@@ -7,12 +7,83 @@ import { GeneratedIdentifierFlags } from 'typescript'
 
 // Components
 import { BackgroundImage1, BackgroundImage2, FooterCon,GradientBackgroundCon, FooterLink, RedSpan, QuoteGeneratorTitle, QuoteGeneratorInnerCon, GenerateQuoteButton, GenerateQuoteButtonText, QuoteGeneratorCon, QuoteGeneratorSubTitle } from '@/Components/QuoteGenerator/QuoteGeneratorElements'
-
+import QuoteGeneratorModal from '@/Components/QuoteGenerator'
 // Assets
 import Clouds1 from '../Assets/cloud-and-thunder.png'
 import Clouds2 from '../Assets/cloudy-weather.png'
+import { API } from 'aws-amplify'
+import { quotesQueryName } from '@/src/graphql/queries'
+import { useEffect, useState } from 'react'
+import { GraphQLResult } from '@aws-amplify/api-graphql'
+
+// interface for our DynamoDB object
+interface UpdateQuoteInfoData {
+  id: string;
+  queryName: string;
+  quotesGenerated: number;
+  createdAt: string;
+  updatedAt: string;
+}
+
+// type guard for our fetch function
+function isGraphQLResultForquotesQueryName(response: any): response is GraphQLResult<{
+  quotesQueryName: {
+    items: [UpdateQuoteInfoData];
+  };
+}> {
+  return response.data && response.data.quotesQueryName && response.data.quotesQueryName.items;
+}
+
+
+
 export default function Home() {
   const [numberOfQuotes, setNumberOfQuotes] = useState<Number | null>(0);
+  const [openGenerator, setOpenGenerator] = useState(false);
+  const [processingQuote, setProcessingQuote] = useState(false);
+  const [quoteReceived, setQuoteReceived] = useState<String | null>(null);
+    // Function to fetch our DynamoDB object (quotes generated)
+    const updateQuoteInfo = async () => {
+      try {
+        const response = await API.graphql<UpdateQuoteInfoData>({
+          query: quotesQueryName,
+          authMode: "AWS_IAM",
+          variables: {
+            queryName: "LIVE",
+          },
+        })
+        //console.log('response', response);
+        // Create type guards
+        if (!isGraphQLResultForquotesQueryName(response)) {
+          throw new Error('Unexpected response from API.graphql');
+        }
+
+        if (!response.data) {
+          throw new Error('Response data is undefined');
+        }
+
+        const receivedNumberOfQuotes = response.data.quotesQueryName.items[0].quotesGenerated;
+        setNumberOfQuotes(receivedNumberOfQuotes);
+
+      } catch (error) {
+        console.log('error getting quote data', error)
+      }
+    }
+
+
+    useEffect(() => {
+      updateQuoteInfo();
+    }, [])
+
+      // Functions for quote generator modal
+  const handleCloseGenerator = () => {
+    setOpenGenerator(false);
+    setProcessingQuote(false);
+    setQuoteReceived(null);
+  }
+  const handleOpenGenerator = async (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    setOpenGenerator(true);
+  }
 
   return (
     <>
@@ -24,9 +95,17 @@ export default function Home() {
       </Head>
       {/* Background */}
       <GradientBackgroundCon>
+
+
       {/*Quote Generator Modal*/}
-      {/* <QuoteGeneratorModal
-      />*/}
+       <QuoteGeneratorModal
+          open={openGenerator}
+          close={handleCloseGenerator}
+          processingQuote={processingQuote}
+          setProcessingQuote={setProcessingQuote}
+          quoteReceived={quoteReceived}
+          setQuoteReceived={setQuoteReceived}
+       />
 
           {/* Quote Generator */}
           <QuoteGeneratorCon>
@@ -41,7 +120,9 @@ export default function Home() {
             </QuoteGeneratorSubTitle>
 
             <GenerateQuoteButton>
-              <GenerateQuoteButtonText  onClick={null}>
+              <GenerateQuoteButtonText  
+              onClick={handleOpenGenerator}
+              >
                 Make a Quote
               </GenerateQuoteButtonText>
             </GenerateQuoteButton>
@@ -75,4 +156,4 @@ export default function Home() {
       </GradientBackgroundCon>
     </>
   )
-}
+  }
